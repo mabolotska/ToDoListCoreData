0f48d1b834f011823b04e609cf9e35c9453ef2f5
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-22 11:11:26 +0200

    Initial Commit

---
+++ ToDoListCodeData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCodeData
+//
+//  Created by Maryna Bolotska on 22/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
